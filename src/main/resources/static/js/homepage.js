b6e1789752d0175ea58bfccb92155c90f0a15913
Marcin <poreba.m7@gmail.com>
2018-08-16 17:28:25 +0200

    Add home page

---
+++ src/main/resources/static/js/homepage.js
@@ -0,0 +1,15 @@
+function signInButton() {
+    window.open("../html/signIn.html");
+}
+
+function signOutButton() {
+    var a;
+}
+
+function singlePlayerButton() {
+    var c;
+}
+
+function multiplePlayerButton() {
+    var d;
+}
