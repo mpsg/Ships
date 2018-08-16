d6f4cdd4eec0a748cb7f945ea5e35a055f7a084a
p-m-r <p.m.rduch@gmail.com>
2018-08-16 18:35:08 +0200

    request test/?param1=&param2=

--- src/main/java/com/ships/demo/controller/DemoController.java
+++ src/main/java/com/ships/demo/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.ships.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,5 +10,10 @@ public class DemoController {
     @ResponseBody
     public String login() {
         return "Login";
+    }
+    @RequestMapping(value="/test")
+    @ResponseBody
+    public String test(@RequestParam("param1") String param1, @RequestParam("param2") String param2) {
+        return Integer.toString(Integer.parseInt(param1) + Integer.parseInt(param2));
     }
 }
