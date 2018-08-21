c52d30f93e0246e4b913020a09485d5c74d072d7
p-m-r <p.m.rduch@gmail.com>
2018-08-21 22:36:03 +0200

    added skeleton classes for PositionedShip and ShipPlacer

--- src/main/java/com/ships/demo/Board.java
+++ src/main/java/com/ships/demo/Board.java
@@ -13,14 +13,13 @@ public class Board {
     }
     //ship - the ship to be inserted; x, y - coordinates of a point at the end of the ship, origin for the orientation;
     //orientation - 0 means the ship extends to the right from the origin, 1 to the bottom, 2 to the left, 3 upwards;
-    //returns true if the ship can be inserted in the given position, false if the operation failed
-    public boolean insertShip(Ship ship, int x, int y, int orientation) {
-        //TODO implement method
-        return true;
+    public void insertShips(Vector<PositionedShip> ships) {
+        ShipPlacer shipPlacer = new ShipPlacer(this);
+        shipPlacer.placeShips(ships);
     }
     //x, y - coordinates of the hit; returns 0 if it's a miss, 1 if it's a hit, 2 if the ship was hit and destroyed
     public int shootAt(int x, int y) {
-        if (board[x][y] == null) {
+        if (this.board[x][y] == null) {
             return 0;
         } else if (!this.board[x][y].makeHit()) {
             this.ships.remove(this.board[x][y]);
---
+++ src/main/java/com/ships/demo/ShipPlacer.java
@@ -0,0 +1,13 @@
+package com.ships.demo;
+
+import java.util.Vector;
+
+public class ShipPlacer {
+    private Board board;
+    public ShipPlacer(Board board) {
+        this.board = board;
+    }
+    public void placeShips(Vector<PositionedShip> ships) {
+        //TODO implement method
+    }
+}
