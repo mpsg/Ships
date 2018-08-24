61a677f8661e4c1babf18923df35942ade2e3034
Marcin <poreba.m7@gmail.com>
2018-08-24 15:46:05 +0200

    js JSON handling

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -3,15 +3,28 @@ function signInButton() {
 }
 
 function signOutButton() {
+    //var xmlHttp = new XMLHttpRequest();
+    //xmlHttp.onreadystatechange = function () {
+    //    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+    //        console.log(xmlHttp.responseText)
+    //    }
+    //}
+    //var message = "/test?&param1=5&param2=2";
+    //xmlHttp.open("GET", message, true);
+    //xmlHttp.send(null);
+
     var xmlHttp = new XMLHttpRequest();
+    var message = "/login?data=" + encodeURIComponent(JSON.stringify({"param1": "2", "param2": "B6"}));
+    xmlHttp.open("GET", message, true);
+    xmlHttp.setRequestHeader("Content-Type", "application/json");
     xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            console.log(xmlHttp.responseText)
+            var json = JSON.parse(message.responseText);
+            console.log(json.param1 + ", " + json.param2);
         }
     }
-    var message = "/test?&param1=5&param2=2";
-    xmlHttp.open("GET", message, true);
-    xmlHttp.send(null);
+
+    xmlHttp.send();
 }
 
 function singlePlayerButton() {
