81ec9bf75e6d35f4a7a7771dfeb7228bd74891dd
p-m-r <p.m.rduch@gmail.com>
2018-08-24 13:40:02 +0200

    code refactoring

--- src/main/java/com/ships/demo/RandomShipPlacer.java
+++ src/main/java/com/ships/demo/RandomShipPlacer.java
@@ -10,26 +10,31 @@ public class RandomShipPlacer extends ShipPlacer {
     }
     public void placeShipsAtRandom(Vector<Ship> ships) {
        for (Ship ship : ships) {
-           boolean shipPlaced = false;
+           boolean shipPlaced;
            do {
                int x = this.getRandomFirstCoordinate();
                int y = this.getRandomSecondCoordinate();
-               if (this.board[x][y] == null) {
-                   int orientation = this.getRandomOrientation();
-                   int checkedOrientations = 0;
-                   do {
-                       if (this.checkIfShipWithinBoard(ship.getSize(), x, y, orientation) &&
-                               this.checkIfDoesNotCollide(ship.getSize(), x, y, orientation)) {
-                           this.placeShip(ship, x, y, orientation);
-                           shipPlaced = true;
-                       } else {
-                           checkedOrientations++;
-                           orientation = orientation == 3 ? 0 : orientation + 1;
-                       }
-                   } while (!shipPlaced && checkedOrientations < 4);
-               }
+               shipPlaced = this.tryToPlaceShip(ship, x, y);
            } while (!shipPlaced);
         }
+    }
+    private boolean tryToPlaceShip(Ship ship, int x, int y) {
+        boolean shipPlaced = false;
+        if (this.board[x][y] == null) {
+            int orientation = this.getRandomOrientation();
+            int checkedOrientations = 0;
+            do {
+                if (this.checkIfShipWithinBoard(ship.getSize(), x, y, orientation) &&
+                        this.checkIfDoesNotCollide(ship.getSize(), x, y, orientation)) {
+                    this.placeShip(ship, x, y, orientation);
+                    shipPlaced = true;
+                } else {
+                    checkedOrientations++;
+                    orientation = orientation == 3 ? 0 : orientation + 1;
+                }
+            } while (!shipPlaced && checkedOrientations < 4);
+        }
+        return shipPlaced;
     }
     private void placeShip(Ship ship, int x, int y, int orientation) {
         this.board[x][y] = new BoardField(ship);
